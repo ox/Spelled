dc55ae70dbbe06f0781bf5fd3e95eea87e8a61e8
Artem Titoulenko <owlhuntr92@gmail.com>
2010-07-11 01:06:38 -0400

    sdasd

--- config/environment.rb
+++ config/environment.rb
@@ -21,7 +21,6 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "hpricot", :version => '0.8.2', :source => "http://gemcutter.org"
   config.gem "rest-open-uri"
-  config.gem "shoulda", :lib => "shoulda"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
