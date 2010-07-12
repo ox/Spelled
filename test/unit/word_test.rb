527d6171717cab4351324b0ef686fd7c74f45613
Artem Titoulenko <owlhuntr92@gmail.com>
2010-07-12 14:21:42 -0400

    Fixed ajax, and other stuff

--- test/unit/word_test.rb
+++ test/unit/word_test.rb
@@ -1,3 +1,11 @@
 require 'test_helper'
-
+require 'active_support'
 #todo
+
+class TestWords < Test::Unit::TestCase
+  must "have wrong word and right word" do
+    word = Word.new(:right => "word", :wrong => "wrod")
+    assert "word", word.right
+    assert "wrod", word.wrong
+  end
+end
